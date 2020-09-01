e6ba1929321e2a5de3d59ee323da8ee13263fbaf
Jeroen Claassens <support@favware.tech>
2020-09-01 17:03:23 +0200

    refactor(i18n): remove ternaries

--- src/lib/structures/MultiArgument.js
+++ src/lib/structures/MultiArgument.js
@@ -47,7 +47,14 @@ class MultiArgument extends Argument {
 		}
 
 		args.push(rest.splice(0, structures.length).join(usageDelim), ...rest);
-		if ((min && structures.length < min) || !structures.length) throw message.language.get(`resolverMultiTooFew`, { name: base.name, min });
+		if ((min && structures.length < min) || !structures.length) {
+			const minimal = !min || min <= 0 ? 1 : min;
+			throw message.language.get(`resolverMultiTooFew`, {
+				name: base.name,
+				min: minimal,
+				conjunctionWord: message.language.get(minimal === 1 ? 'globalIs' : 'globalAnd')
+			});
+		}
 		return structures;
 	}
 }
