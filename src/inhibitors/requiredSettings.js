674ce253ec653afb82929832b5f7750536b86616
Favna <contact@skyra.pw>
2020-09-14 10:37:55 +0000

    build: output for bcc805c291c560f642e3d375170f731e24dcea2c

--- src/inhibitors/requiredSettings.js
+++ src/inhibitors/requiredSettings.js
@@ -5,6 +5,10 @@ module.exports = class extends Inhibitor {
 		if (!command.requiredSettings.length || message.channel.type !== 'text') return;
 		// eslint-disable-next-line eqeqeq, no-eq-null
 		const requiredSettings = command.requiredSettings.filter((setting) => message.guild.settings.get(setting) == null);
-		if (requiredSettings.length) throw message.language.get('inhibitorRequiredSettings', { settings: requiredSettings });
+		if (requiredSettings.length)
+			throw message.language.get(requiredSettings.length === 1 ? 'inhibitorRequiredSettings' : 'inhibitorRequiredSettingsPlural', {
+				settings: requiredSettings.join(', '),
+				count: requiredSettings.length
+			});
 	}
 };
