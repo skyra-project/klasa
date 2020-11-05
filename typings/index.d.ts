67f5aff0fd5ecb1eca27280ae1202f53065301d4
Antonio Román <kyradiscord@gmail.com>
2020-11-05 14:47:49 +0100

    fix(types): corrected PermissionLevels#add type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -73,7 +73,7 @@ declare module 'klasa' {
 	export class PermissionLevels extends Collection<number, PermissionLevel> {
 		public constructor(levels?: number);
 
-		public add(level: number, check: (message: KlasaMessage) => boolean, options?: PermissionLevelOptions): this;
+		public add(level: number, check: (message: KlasaMessage) => boolean | Promise<boolean>, options?: PermissionLevelOptions): this;
 		public debug(): string;
 		public isValid(): boolean;
 		public remove(level: number): this;
